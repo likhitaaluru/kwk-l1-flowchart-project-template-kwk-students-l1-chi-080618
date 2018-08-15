79345d494d7e7ce269483bd489f9024a2f614cdb
likhitaaluru <likhitaaluru@gmail.com>
2018-08-15 21:33:52 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,30 +18,37 @@ class ApplicationController < Sinatra::Base
     commitment = params[:commitment]
     city = params[:city]
 
+    decision = []
     if place == "virtual"
       if price == "low"
         if commitment == "short"
           # if city == Chicago
-          return "https://www.udemy.com/street-safety-for-parents-of-teens-young-adults/"
+          decision << "a udemy web page instructing teens street safety"
+          decision << "https://www.udemy.com/street-safety-for-parents-of-teens-young-adults/"
         else commitment == "long"
           # if city == Chicago
-          return "https://www.youtube.com/channel/UCj4JOZ3jvgkHPYew0ksfDZw"
+          decision << "a youTube channel meant to instruct on self defense"
+          decision << "https://www.youtube.com/channel/UCj4JOZ3jvgkHPYew0ksfDZw"
         end
       elsif price == "med"
         if commitment == "short"
           # if city == Chicago
-          return "https://www.protrainings.com/en/courses/210-self-defense?topic_id=8-self-defense"
+          decision << "one hour video course from ProTrainings"
+          decision << "https://www.protrainings.com/en/courses/210-self-defense?topic_id=8-self-defense"
         else commitment == "long"
           # if city == Chicago
-          return "http://www.s2online.academy/courses/fighting-chance-womens-self-defense-course"
+          decision << "searching the S2 Academy courses."
+          decision << "http://www.s2online.academy/courses/fighting-chance-womens-self-defense-course"
         end
       else price == "high"
         if commitment == "short"
           # if city == Chicago
-          return "none availiable!"
+          decision << "searching for one on your own"
+          decision << "we are unable to find one with that criterion"
         else commitment == "long"
           # if city == Chicago
-          return "https://www.selfdefensecollege.com/"
+          decision << "buying a self defense study kit from the Self Defense College."
+          decision << "https://www.selfdefensecollege.com/"
         end
       end
       
@@ -49,26 +56,32 @@ class ApplicationController < Sinatra::Base
       if price == "low"
         if commitment == "short"
           # if city == Chicago
-          return "https://today.uic.edu/uic-police-offer-womens-self-defense-courses"
+          decision << "attending a session offered free from the University of Illinois at Chicago police."
+          decision << "https://today.uic.edu/uic-police-offer-womens-self-defense-courses"
         else commitment == "long"
           # if city == Chicago
-          return "https://www.iskc.com/"
+          decision << "buying a membership to the IL Shotokan Karate Clubs."
+          decision << "https://www.iskc.com/"
         end
       elsif price == "med"
         if commitment == "short"
           # if city == Chicago
-          return "http://www.orangeshoe.com/Lakeview/PersonalTrainerCosts "
+          decision << "investing in a personal trainer."
+          decision << "http://www.orangeshoe.com/Lakeview/PersonalTrainerCosts "
         else commitment == "long"
           # if city == Chicago
-          return "https://www.9round.com/"
+          decision << "investing in a membership to 9round."
+          decision << "https://www.9round.com/"
         end
       elsif price == "high"
         if commitment == "short"
           # if city == Chicago
-          return "http://www.impactchicago.org/"
+          decision << "enrolling in an Impact Chicago workshop."
+          decision << "http://www.impactchicago.org/"
         else commitment == "long"
           # if city == Chicago
-          return "http://www.chicagomma.com/?gclid=CjwKCAjwns_bBRBCEiwA7AVGHmC1TKLENP4Qhsaq72LVqYDhlg-tedpj5uZNZuhfatuzC2cUUyyEHRoCctsQAvD_BwE#mixed-martial-arts"
+          decision << "becoming a member of the Chicago MMA branch."
+          decision << "http://www.chicagomma.com/?gclid=CjwKCAjwns_bBRBCEiwA7AVGHmC1TKLENP4Qhsaq72LVqYDhlg-tedpj5uZNZuhfatuzC2cUUyyEHRoCctsQAvD_BwE#mixed-martial-arts"
         end
       end
 
@@ -76,6 +89,7 @@ class ApplicationController < Sinatra::Base
     else
       return "wrong page"
     end
+    return "Due to your selections, we would recommend a #{decision[0]} (#{decision[1]})."
     
     
   end
