1aedbe3e7a347d1ac3f06ced281e089dcbeced49
likhitaaluru <likhitaaluru@gmail.com>
2018-08-15 20:00:20 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,31 @@
+require './config/environment'
+
 class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+  end
+  
+  get '/' do
+    erb :index
+  end
+  
+  post '/results' do
+    
+    place = params[:place]
+    price = params[:price] 
+    commitement = params[:commitement]
+    city = params[:city]
+    
+    if place == "virtual"
+      return "virtual page"
+    elsif place == "physical"
+      return "physical page"
+    else
+      return "wrong page"
+    end
+    
   end
 
 end
