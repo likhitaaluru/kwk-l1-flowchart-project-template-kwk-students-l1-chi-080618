8a9e4b20a476ba0b919780a842442f0b17ad640a
likhitaaluru <likhitaaluru@gmail.com>
2018-08-17 02:13:20 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,31 +22,31 @@ class ApplicationController < Sinatra::Base
     if place == "virtual"
       if price == "low"
         if commitment == "short"
-          if city == Chicago
+          # if city == Chicago
           decision << "a udemy web page instructing teens street safety"
           decision << "https://www.udemy.com/street-safety-for-parents-of-teens-young-adults/"
         else commitment == "long"
-          if city == Chicago
+          # if city == Chicago
           decision << "a youTube channel meant to instruct on self defense"
           decision << "https://www.youtube.com/channel/UCj4JOZ3jvgkHPYew0ksfDZw"
         end
       elsif price == "med"
         if commitment == "short"
-          if city == Chicago
+          # if city == Chicago
           decision << "one hour video course from ProTrainings"
           decision << "https://www.protrainings.com/en/courses/210-self-defense?topic_id=8-self-defense"
         else commitment == "long"
-          if city == Chicago
+          # if city == Chicago
           decision << "searching the S2 Academy courses."
           decision << "http://www.s2online.academy/courses/fighting-chance-womens-self-defense-course"
         end
       else price == "high"
         if commitment == "short"
-          if city == Chicago
+          # if city == Chicago
           decision << "searching for one on your own"
           decision << "we are unable to find one with that criterion"
         else commitment == "long"
-          if city == Chicago
+          # if city == Chicago
           decision << "buying a self defense study kit from the Self Defense College."
           decision << "https://www.selfdefensecollege.com/"
         end
@@ -55,31 +55,31 @@ class ApplicationController < Sinatra::Base
     elsif place == "physical"
       if price == "low"
         if commitment == "short"
-          if city == Chicago
+          # if city == Chicago
           decision << "attending a session offered free from the University of Illinois at Chicago police."
           decision << "https://today.uic.edu/uic-police-offer-womens-self-defense-courses"
         else commitment == "long"
-          if city == Chicago
+          # if city == Chicago
           decision << "buying a membership to the IL Shotokan Karate Clubs."
           decision << "https://www.iskc.com/"
         end
       elsif price == "med"
         if commitment == "short"
-          if city == Chicago
+          # if city == Chicago
           decision << "investing in a personal trainer."
           decision << "http://www.orangeshoe.com/Lakeview/PersonalTrainerCosts "
         else commitment == "long"
-          if city == Chicago
+          # if city == Chicago
           decision << "investing in a membership to 9round."
           decision << "https://www.9round.com/"
         end
       elsif price == "high"
         if commitment == "short"
-          if city == Chicago
+          # if city == Chicago
           decision << "enrolling in an Impact Chicago workshop."
           decision << "http://www.impactchicago.org/"
         else commitment == "long"
-          if city == Chicago
+          # if city == Chicago
           decision << "becoming a member of the Chicago MMA branch."
           decision << "http://www.chicagomma.com/?gclid=CjwKCAjwns_bBRBCEiwA7AVGHmC1TKLENP4Qhsaq72LVqYDhlg-tedpj5uZNZuhfatuzC2cUUyyEHRoCctsQAvD_BwE#mixed-martial-arts"
         end
